a07a0e808954672947b3a9972f05258fad9f76d3
joao magnago <joaomagnago.wm@gmail.com>
2023-11-01 20:01:49 -0300

    feat: add react hook form context to form and remaining variables to cycles context

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -1,30 +1,11 @@
-import { zodResolver } from '@hookform/resolvers/zod'
 import { useContext } from 'react'
-import { useForm } from 'react-hook-form'
-import * as zod from 'zod'
+import { useFormContext } from 'react-hook-form'
 import { CyclesContext } from '../..'
 import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
 
-const newCycleFormValidationSchema = zod.object({
-  task: zod.string().min(1, 'Informe a tarefa'),
-  minutesAmount: zod
-    .number()
-    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos.')
-    .max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
-})
-
-type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
-
 export function NewCycleForm() {
   const { activeCycle } = useContext(CyclesContext)
-
-  const { register } = useForm<NewCycleFormData>({
-    resolver: zodResolver(newCycleFormValidationSchema),
-    defaultValues: {
-      task: '',
-      minutesAmount: 0,
-    },
-  })
+  const { register } = useFormContext()
 
   return (
     <FormContainer>
